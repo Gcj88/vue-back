cc91dee972316ba3cd00a9315394ec390e547570
高仓健 <gaocangjian@bosssoft.com.cn>
2021-12-25 18:55:44 +0800

    update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let http = require('http');
 let users = [
     {id : 11,name : 'gaocangjian11'},
-    {id : 22,name : 'gaocangjian22'},
-    {id : 33,name : 'gaocangjian33'}
+    {id : 11,name : 'gaocangjian22'},
+    {id : 11,name : 'gaocangjian33'}
 ]
 let server = http.createServer(function(req,res){
     res.setHeader("Access-Control-Allow-Origin","*");
